539a9530dc1593fac4fbc081bc53b7f193256590
InzaSanam08 <inzasanam7@gmail.com>
2026-04-06 21:12:27 +0500

    login signup api

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const connectDB = require('./config/db');
-// const authRoutes = require('./routes/authRoutes');
-
-// dotenv.config();
-// connectDB(); // Database connect karein
-
-// const app = express();
-// app.use(express.json()); // Body parser
-
-// // Routes
-// app.use('/api/auth', authRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
